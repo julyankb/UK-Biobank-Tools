303044a46e342f1b647b016b8803163b4b78c1b7
julyankb <julyanbaruch@gmail.com>
2018-10-05 14:45:12 -0400

    first commit

---
+++ phenotype-database-builder/build_db.py
@@ -0,0 +1,94 @@
+from __future__ import division
+import sqlite3
+import sys
+import os
+import gzip
+import subprocess
+import time
+import scrape_ukb
+
+start_time = time.time()
+
+# USER INPUTS
+database_dir = 'target directory'
+database_name = 'UKBB.db'
+data_file = 'ukb_phenofile.tab' # Tab-separated UKB phenotype file.
+
+# GENERATE LIST OF COLUMNS
+proc = subprocess.Popen(['head', '-1', data_file], stdout=subprocess.PIPE)
+output = proc.stdout.read()
+columns = [c for c in output.split('\t') if c[0] == 'f' and '\n' not in c]
+
+# CREATE DATABASE IF NOT EXISTS, INITIALIZE CURSOR
+conn = sqlite3.connect(database_dir + database_name)
+c = conn.cursor()
+
+# DEFINE FUNCTIONS
+def create_tables():
+        c.execute('CREATE TABLE IF NOT EXISTS participants (rowid integer primary key autoincrement, eid, df, instnum, itemnum, value)')
+	c.execute('CREATE TABLE IF NOT EXISTS dfdesc (df integer primary key, desc, category, type)')
+	
+def insert_participant_data(line):
+	data = line.split('\t')
+	eid = int(data[0])
+	for i in range(1, len(columns)):
+		column = columns[i].split('.') # of the form [f, 21, 0, 0]
+		print column
+		df, instnum, itemnum, value = int(column[1]), int(column[2]), int(column[3]), data[i]
+		if value == 'NA': # 1 Byte to store NULL, 2 Bytes to store NA.
+			value = None
+		c.execute("INSERT INTO participants VALUES (?,?,?,?,?,?)", (None, eid, df, instnum, itemnum, value))
+	conn.commit()
+	
+def insert_dfdesc_data(line):
+	data = line.split('\t')
+	df, desc, category, df_type = int(data[0]), data[1], data[2], data[3]
+	c.execute("INSERT INTO dfdesc VALUES (?,?,?,?)", (df, desc, category, df_type))
+	conn.commit()	
+
+def read_from_db(query):
+	c.execute(query)
+	for row in c.fetchall():
+		 print row
+
+# ---------------------------------------------------------------------------------------------------
+# ALWAYS RUN
+create_tables()
+
+# ---------------------------------------------------------------------------------------------------
+# INSERT INTO patients TABLE
+run_below = 0
+if run_below:
+	with open(data_file) as infile:
+		next(infile) #skip header
+		for counter, line in enumerate(infile):
+			insert_participant_data(line)
+			print "Inserted participant %s \t f.eid = %s "%(counter, line.split('\t')[0])
+
+# CREATE participants.df INDEX
+run_below = 0
+if run_below:
+	print "Creating index on participants.df..."
+        c.execute("CREATE INDEX IF NOT EXISTS df_index ON participants (df)")
+	print "Process completed."
+
+# CREATE INDEX ON participants.eid
+run_below = 0
+if run_below:
+	print "Creating index on participants.eid..."
+	c.execute("CREATE INDEX IF NOT EXISTS eid_index ON participants (eid)")
+	print "Process completed."
+
+# ---------------------------------------------------------------------------------------------------
+# INSERT INTO dfdesc TABLE 
+run_below = 0
+if run_below:
+	df_descriptions = scrape.df_descriptions
+	for line in df_descriptions:
+		#print line
+		insert_dfdesc_data(line)
+		print "Inserted df %s"%line.split('\t')[0]	
+
+# ---------------------------------------------------------------------------------------------------
+# PRINT TIME ELAPSED
+print 'Time elapsed:', time.time() - start_time
---
+++ phenotype-database-builder/scrape_ukb.py
@@ -0,0 +1,40 @@
+import bs4 as bs
+from urllib2 import urlopen
+import unicodedata
+
+sauce = urlopen('http://biobank.ctsu.ox.ac.uk/crystal/list.cgi').read()
+soup = bs.BeautifulSoup(sauce, 'lxml')
+
+field_listing = ['Integer',
+		'Categorical (single)',
+		'Categorical (multiple)',
+		'Continuous',
+		'Text',
+		'Date',
+		'Time',
+		'Compound']
+
+df_descriptions = []
+
+for url in soup.find_all('a'):
+	if url.text in field_listing:
+		df_type = url.text
+		link = 'http://biobank.ctsu.ox.ac.uk/crystal/' +  url.get('href')
+		sauce_loop = urlopen(link).read()
+		soup_loop = bs.BeautifulSoup(sauce_loop, 'lxml')
+		raw_text = unicodedata.normalize("NFKD", soup_loop.get_text('\t'))
+		lines = raw_text.split('\n')
+		for line in lines:
+			splt = [s for s in line.split('\t')[1:] if s != '' 
+							and s != ' ' 
+							and s!= '  ' 
+							and s!= u'\u2020'] 
+							#u'\u2020' is the dagger from Compound
+			if len(splt) == 3 and splt[0].isdigit():
+				splt.append(df_type)
+				df_descriptions.append('%s\t%s\t%s\t%s'%(splt[0], splt[1], splt[2], splt[3]))
+
+if __name__ == "__main__":
+	for f in df_descriptions:
+		#print '%s\t%s\t%s\t%s'%(f[0],f[1],f[2],f[3])
+		print f
